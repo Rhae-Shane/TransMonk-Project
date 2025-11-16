6c3a6f588288af87b2a3034be7eff977c4c06b2e
Rhae-Shane <omeshkumar9813499778@gmail.com>
2025-11-16 21:19:21 +0530

    api done

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:5000";
+const API = "https://transmonk-project.onrender.com";
 
 export default function Admin() {
   const [products, setProducts] = useState([]);
